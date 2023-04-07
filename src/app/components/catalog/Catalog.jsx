942d095b4cff3b651a9e2d96e60800afc2524912
Claret Devigne <claretdevigne@gmail.com>
2023-04-07 02:12:05 -0500

    Add styles into Catalog component

--- src/app/components/catalog/Catalog.jsx
+++ src/app/components/catalog/Catalog.jsx
@@ -2,19 +2,39 @@ import Product from "../product/Product"
 
 export default function Catalog ( props ) {
 
+  const styles = {
+    h2: {
+      fontSize: "3rem",
+      textAlign: "center",
+      marginTop: "2rem"
+    },
+
+    productContainer: {
+      margin: "2rem 10%",
+      display: "flex",
+      flexWrap: "wrap",
+      gap: "5%",
+      justifyContent: "center"
+    }
+  }
+
   return (
     <>
-      <h2>Catálogo</h2>
+      <h2 style={ styles.h2 }>Catálogo</h2>
       {
         (!props.data) ?
         <p>No products</p> :
-        props.data.map((i, k) => <Product 
-          key={ k } 
-          src={ i.src } 
-          alt={ i.alt } 
-          title={ i.title }
-          price={ i.price }
-        />)
+        <div style={ styles.productContainer }>
+          {
+            props.data.map((i, k) => <Product 
+              key={ k } 
+              src={ i.src } 
+              alt={ i.alt } 
+              title={ i.title }
+              price={ i.price }
+            />) 
+          }
+        </div>
       }
     </>
   )
